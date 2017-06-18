ac5168b0f13be13a22f912e33c81a1546272a4ed
allipierre <yottiallipierre40@yahoo.fr>
2017-06-18 15:04:09 +0200

    hh

--- src/main/java/io/blackground/jobfinder/services/CompanyService.java
+++ src/main/java/io/blackground/jobfinder/services/CompanyService.java
@@ -45,7 +45,7 @@ public class CompanyService {
 
 	public List<Company> findAll() {
 		List<Company> jobs = new ArrayList<>();
-		for (Company job : companyRepository.findJoin()) {
+		for (Company job : companyRepository.findAll()) {
 			jobs.add(job);
 		}
 		return jobs;
