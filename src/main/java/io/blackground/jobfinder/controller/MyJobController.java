aabbb3073ed2f4d6ba61e202c075d5bb837dc528
allipierre <yottiallipierre40@yahoo.fr>
2017-06-19 16:56:35 +0200

    hh

--- src/main/java/io/blackground/jobfinder/controller/MyJobController.java
+++ src/main/java/io/blackground/jobfinder/controller/MyJobController.java
@@ -6,12 +6,18 @@ package io.blackground.jobfinder.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.blackground.jobfinder.models.Company;
+import io.blackground.jobfinder.models.User;
+import io.blackground.jobfinder.services.CompanyService;
 import io.blackground.jobfinder.services.ContractService;
 import io.blackground.jobfinder.services.ExperienceService;
 import io.blackground.jobfinder.services.JobService;
+import io.blackground.jobfinder.services.UserServiceImpl;
 
 /**
  * @author yotti
@@ -19,11 +25,13 @@ import io.blackground.jobfinder.services.JobService;
  */
 @Controller
 public class MyJobController {
-
-	
+	@Autowired
+	private UserServiceImpl userService;
+	@Autowired
+	private CompanyService companyService;
 	@Autowired
 	private ContractService contractservice;
-	
+
 	@Autowired
 	private ExperienceService experienceservice;
 
@@ -32,7 +40,13 @@ public class MyJobController {
 
 	@GetMapping("/myjob")
 	public String postJob(HttpServletRequest request) {
-		request.setAttribute("jobs", jobService.findAll());
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		User user = userService.findByUsername(authentication.getName());
+		Company userCompany = companyService.findCompany(user);
+
+		request.setAttribute("jobs", jobService.findJobById(userCompany.getId()));
 		request.setAttribute("contract", contractservice.findAll());
 		request.setAttribute("experience", experienceservice.findAll());
 		return "myjob";
