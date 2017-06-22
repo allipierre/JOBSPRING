2bd078abb33f0bebbc8088a84d17c14498573c16
allipierre <yottiallipierre40@yahoo.fr>
2017-06-22 18:39:52 +0200

    hh

--- src/main/java/io/blackground/jobfinder/controller/RestController.java
+++ src/main/java/io/blackground/jobfinder/controller/RestController.java
@@ -3,22 +3,41 @@
  */
 package io.blackground.jobfinder.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import io.blackground.jobfinder.models.Job;
 
 /**
  * @author yotti
  *
  */
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/contacts")
 public class RestController {
-//
-//	@GetMapping("/index")
-//	public String hello() {
-//		return "index";
-//	}
+	@RequestMapping(method=RequestMethod.GET)
+	  public List<Job> getAll() {
+	    return new ArrayList<Job>();
+	  }
+	  
+	  @RequestMapping(method=RequestMethod.POST)
+	  public Job create(@RequestBody Job contact) {
+	    return null;
+	  }
+	  
+	  @RequestMapping(method=RequestMethod.DELETE, value="{id}")
+	  public void delete(@PathVariable String id) {
 
+	  }
+	  
+	  @RequestMapping(method=RequestMethod.PUT, value="{id}")
+	  public Job update(@PathVariable String id, @RequestBody Job contact) {
+	  return null;
+	  }
 
-
-}
+	}
