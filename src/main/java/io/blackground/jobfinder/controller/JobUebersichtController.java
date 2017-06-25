badde4ee24e02961707ad7b22fc4e0c643e44ff9
allipierre <yottiallipierre40@yahoo.fr>
2017-06-25 14:14:17 +0200

    gg

---
+++ src/main/java/io/blackground/jobfinder/controller/JobUebersichtController.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package io.blackground.jobfinder.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import io.blackground.jobfinder.models.Company;
+
+import io.blackground.jobfinder.models.Job;
+import io.blackground.jobfinder.models.User;
+import io.blackground.jobfinder.services.CompanyService;
+
+import io.blackground.jobfinder.services.ContractService;
+
+import io.blackground.jobfinder.services.ExperienceService;
+import io.blackground.jobfinder.services.IndustryService;
+import io.blackground.jobfinder.services.JobService;
+import io.blackground.jobfinder.services.SalaryService;
+import io.blackground.jobfinder.services.UserServiceImpl;
+
+/**
+ * @author yotti
+ *
+ */
+@Controller
+public class JobUebersichtController {
+
+
+	@GetMapping("/jobuebersicht")
+	public String postJob(HttpServletRequest request) {
+		return "jobuebersicht";
+	}
+
+}
