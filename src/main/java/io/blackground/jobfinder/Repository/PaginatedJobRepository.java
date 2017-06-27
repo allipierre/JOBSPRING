a53ca40e724b8f02bb77e3b95399d263e9b589a8
allipierre <yottiallipierre40@yahoo.fr>
2017-06-27 15:15:35 +0200

    gg

--- src/main/java/io/blackground/jobfinder/Repository/PaginatedJobRepository.java
+++ src/main/java/io/blackground/jobfinder/Repository/PaginatedJobRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import io.blackground.jobfinder.models.Job
+import io.blackground.jobfinder.models.Job;
 
 /**
  * @author yotti
