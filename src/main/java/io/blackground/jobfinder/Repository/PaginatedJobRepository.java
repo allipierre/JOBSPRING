ab4acfb939fd03e430b43a0d6adf217902ecadf5
allipierre <yottiallipierre40@yahoo.fr>
2017-06-27 15:12:37 +0200

    gg

--- src/main/java/io/blackground/jobfinder/Repository/PaginatedJobRepository.java
+++ src/main/java/io/blackground/jobfinder/Repository/PaginatedJobRepository.java
@@ -3,8 +3,6 @@
  */
 package io.blackground.jobfinder.Repository;
 
-
-
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -14,22 +12,23 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import io.blackground.jobfinder.models.Job;
 
-
-
 /**
  * @author yotti
  *
  */
 public interface PaginatedJobRepository extends PagingAndSortingRepository<Job, Long> {
 	Page<Job> findAll(Pageable pageable);
-	List<Job> findJobsByTitle(String title,Pageable pageable);
-	List<Job> findByTitleContainingIgnoreCase(String title,Pageable pageable);
-	List<Job> findJobsByCompanyCityContainingIgnoreCase(String city,Pageable pageable);
-	List<Job> findJobsByCompanyCityContainingIgnoreCaseAndTitleContainingIgnoreCase(String city,String title,Pageable pageable);
-	
-	
-	@Query(value = "SELECT * FROM Job j join Company c WHERE c.id= ?1",
-		    countQuery = "SELECT count(*) FROM job j join Company c WHERE c.id= ?1",
-		    nativeQuery = false)
-		  Page<Job> findByCompanyId(Long companyId, Pageable pageable);
+
+	List<Job> findJobsByTitle(String title, Pageable pageable);
+
+	List<Job> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+	List<Job> findJobsByCompanyCityContainingIgnoreCase(String city, Pageable pageable);
+
+	List<Job> findJobsByCompanyCityContainingIgnoreCaseAndTitleContainingIgnoreCase(String city, String title,
+			Pageable pageable);
+
+	@Query(value = "SELECT c.companyName,c.website,c.about,c.city,j.position,j.id,j.description,j.minimumExperienceYears,j.category,j.minimumDegree,j.title,j.contrat,j.published"
+			+ "FROM Job j join Company c WHERE c.id= ?1", nativeQuery = false)
+	Page<Job> findByCompanyId(Long companyId, Pageable pageable);
 }
