a355e9facf6ec191b07dfc6f25460a6865a79433
allipierre <yottiallipierre40@yahoo.fr>
2017-06-27 13:20:02 +0200

    gg

--- src/main/java/io/blackground/jobfinder/services/PaginatedJobService.java
+++ src/main/java/io/blackground/jobfinder/services/PaginatedJobService.java
@@ -54,19 +54,19 @@ public class PaginatedJobService {
 		return paginatedJobRepository.findAll(pageable);
 	}
 
-	public List<Job> findJobByCompany(Pageable pageable) {
+	public Page<Job> findJobByCompany(Pageable pageable) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		User user = userService.findByUsername(authentication.getName());
 		Company userCompany = companyService.findCompany(user);
 		List<Job> jobs = new ArrayList<>();
-		for (Job job : paginatedJobRepository.findAll()) {
+		for (Job job : paginatedJobRepository.findAll(pageable)) {
 			if (job.getCompany().getId() == userCompany.getId()) {
 				jobs.add(job);
 			}
 
 		}
-		return jobs;
+		return paginatedJobRepository.findAll(pageable);
 
 	}
 
