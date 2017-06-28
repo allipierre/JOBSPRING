a2c174e2bde9d8609d0cc509b9b4717aa4a2ced7
allipierre <yottiallipierre40@yahoo.fr>
2017-06-28 14:50:24 +0200

    ggdddd

---
+++ src/main/java/io/blackground/jobfinder/controller/ChatController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package io.blackground.jobfinder.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.mail.MailException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.blackground.jobfinder.models.Company;
+import io.blackground.jobfinder.models.User;
+import io.blackground.jobfinder.services.MailService;
+import io.blackground.jobfinder.services.UserServiceImpl;
+
+/**
+ * @author yotti
+ *
+ */
+@Controller
+public class ChatController {
+
+	@GetMapping("/chat")
+	public String createCompany(HttpServletRequest request) {
+
+		return "chat";
+	}
+}
