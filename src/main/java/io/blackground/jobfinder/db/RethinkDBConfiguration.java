ca7ed4e5b1f1b59c2ac19ba8607126b5c9ce4613
allipierre <yottiallipierre40@yahoo.fr>
2017-06-28 10:59:30 +0200

    gg

--- src/main/java/io/blackground/jobfinder/db/RethinkDBConfiguration.java
+++ src/main/java/io/blackground/jobfinder/db/RethinkDBConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RethinkDBConfiguration {
     // connect to docker
-    public static final String DBHOST = "192.168.0.227";
+    public static final String DBHOST = "localhost";
 
     @Bean
     public RethinkDBConnectionFactory connectionFactory() {
