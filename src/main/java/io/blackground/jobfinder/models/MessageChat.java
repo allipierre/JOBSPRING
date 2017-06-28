945c53569e998d2f5d5a5deeedfff1c7c1b32bf0
allipierre <yottiallipierre40@yahoo.fr>
2017-06-28 14:13:51 +0200

    gg

--- src/main/java/io/blackground/jobfinder/models/MessageChat.java
+++ src/main/java/io/blackground/jobfinder/models/MessageChat.java
@@ -12,94 +12,26 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 import org.springframework.data.annotation.Id;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class MessageChat {
 
-	
-	private long id;
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 
 	private String message;
 	private Date time;
 	private boolean response;
 
 	private transient Company company;
-
-	/**
-	 * @return the id
-	 */
-	@Id
-	@GeneratedValue
-	public long getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the message
-	 */
-	public String getMessage() {
-		return message;
-	}
-
-	/**
-	 * @param message the message to set
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	/**
-	 * @return the time
-	 */
-	public Date getTime() {
-		return time;
-	}
-
-	/**
-	 * @param time the time to set
-	 */
-	public void setTime(Date time) {
-		this.time = time;
-	}
-
-	/**
-	 * @return the response
-	 */
-	public boolean isResponse() {
-		return response;
-	}
-
-	/**
-	 * @param response the response to set
-	 */
-	public void setResponse(boolean response) {
-		this.response = response;
-	}
-
-	/**
-	 * @return the company
-	 */
-	public Company getCompany() {
-		return company;
-	}
-
-	/**
-	 * @param company the company to set
-	 */
-	public void setCompany(Company company) {
-		this.company = company;
-	}
-	
 	
 }
